9dc94d67f3eb4dba11c59ce9063b10f4a89e4557
Daksh Patel <daksh21697@gmail.com>
2024-10-15 14:31:25 -0700

    Removed sns

--- create_data.py
+++ create_data.py
@@ -2,7 +2,6 @@ import time
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
-import seaborn as sns
 from implementations import all_implementations
 
 start_total = time.time()
